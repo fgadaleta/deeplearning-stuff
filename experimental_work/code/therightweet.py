fe023aeeb2ef8a58cc9e5eaf69fc9413674be4d0
fgadaleta <francesco.gadaleta@gmail.com>
2018-09-04 08:37:35 +0200

    Update therightweet.py

--- experimental_work/code/therightweet.py
+++ experimental_work/code/therightweet.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = '1zIV7bf4ERZY1cUOA68gr7HAi'
-consumer_secret = 'dqpGc4yzRpLCHQ3Kh3QFvo4L7vVzyaE4z9Vu9QsbE3mVbjV6Q4' 
-access_token = '333412049-HF1xjZ42pFAX5SqkVcoRvLB7uGba4LGRa1TKlNUy'
-access_token_secret = 'ul0bwY0efpJT1FcId7P4LONY2tnvWdkXgZDraG29MMxDr'
+consumer_key = 'xxx'
+consumer_secret = 'xxx' 
+access_token = 'xxx'
+access_token_secret = 'xxx'
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
